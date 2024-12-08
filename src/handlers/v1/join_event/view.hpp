d3c7e51b5e7370a4ce946da5ace7ffd6a16948d6
Sardor <iamsardorabduraimov@yandex.ru>
2024-12-08 14:08:00 +0500

    Do join event

---
+++ src/handlers/v1/join_event/view.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <userver/components/component_list.hpp>
+
+namespace cooplan {
+
+void AppendJoinEvent(userver::components::ComponentList& component_list);
+    
+}  // namespace bookmarker
