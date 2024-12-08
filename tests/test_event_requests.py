54747d7ec6cea02a590ed5d0e6b06b0e46906bac
Azamat <azamatoff.developer@yandex.com>
2024-12-08 12:42:59 +0500

    Fix: pycodestyle

--- tests/test_event_requests.py
+++ tests/test_event_requests.py
@@ -10,24 +10,35 @@ async def test_get_events(service_client):
     assert response.status == 200
     assert response.json() != {}
 
+
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
 async def test_get_events_by_id(service_client):
     response = await service_client.get('/event/1')
     assert response.status == 200
     assert response.json != {}
 
+
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
 async def test_response_to_join_approve(service_client):
     token = 'asdf'
     headers = {"X-Ya-User-Ticket": token}
-    data = {'user_id': 1, 'event_id': 1, 'status': "accepted"}
-    response = await service_client.post('/response_to_join', headers = headers, json = data)
+    json = {'user_id': 1, 'event_id': 1, 'status': "accepted"}
+    response = await service_client.post(
+        '/response_to_join',
+        headers=headers,
+        json=json
+    )
     assert response.status == 200
+
 
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
 async def test_response_to_join_deny(service_client):
     token = 'asdf'
     headers = {"X-Ya-User-Ticket": token}
-    data = {'user_id': 1, 'event_id': 1, 'status': "rejected"}
-    response = await service_client.post('/response_to_join', headers = headers, json = data)
+    json = {'user_id': 1, 'event_id': 1, 'status': "rejected"}
+    response = await service_client.post(
+        '/response_to_join',
+        headers=headers,
+        json=json
+    )
     assert response.status == 200
