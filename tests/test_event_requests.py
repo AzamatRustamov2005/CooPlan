ca6a54424f230a0dca1cf660cc06ae35ab7085b8
abdulbosit <abdulbositkhalikov96@gmail.com>
2024-12-08 11:51:38 +0500

    New tests

--- tests/test_event_requests.py
+++ tests/test_event_requests.py
@@ -6,13 +6,13 @@ from testsuite.databases import pgsql
 # Start the tests via `make test-debug` or `make test-release`
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
 async def test_get_events(service_client):
-    response = await service_client.get('/v1/events')
+    response = await service_client.get('/events')
     assert response.status == 200
     assert response.json() != {}
 
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
 async def test_get_events_by_id(service_client):
-    response = await service_client.get('/v1/event/1')
+    response = await service_client.get('/event/1')
     assert response.status == 200
     assert response.json != {}
 
@@ -21,7 +21,7 @@ async def test_response_to_join_approve(service_client):
     token = 'asdf'
     headers = {"X-Ya-User-Ticket": token}
     data = {'user_id': 1, 'event_id': 1, 'status': "accepted"}
-    response = await service_client.post('/v1/response_to_join', headers = headers, json = data)
+    response = await service_client.post('/response_to_join', headers = headers, json = data)
     assert response.status == 200
 
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
@@ -29,5 +29,5 @@ async def test_response_to_join_deny(service_client):
     token = 'asdf'
     headers = {"X-Ya-User-Ticket": token}
     data = {'user_id': 1, 'event_id': 1, 'status': "rejected"}
-    response = await service_client.post('/v1/response_to_join', headers = headers, json = data)
+    response = await service_client.post('/response_to_join', headers = headers, json = data)
     assert response.status == 200
